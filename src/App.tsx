d19f678c5fc223513d8ce22baaf62dfc03024c79
timothyrusso <russotimothy@live.it>
2022-07-22 07:53:16 +0200

    feat: header structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './App.css';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: FC = () => {
+  return (
+    <div className="App">
+      <div className="header">
+        <input type="text" placeholder="Task..."></input>
+        <input type="number" placeholder="Deadline (in Days)..."></input>
+      </div>
+      <div className="todoList"></div>
+    </div>
+  );
+};
 
 export default App;
