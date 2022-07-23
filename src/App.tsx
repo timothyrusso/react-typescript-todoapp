f1e5e0d73f6443f2c7481eb6bdec4030ad7b47d0
timothyrusso <russotimothy@live.it>
2022-07-23 12:42:27 +0200

    feat: main application states

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './App.css';
 
 const App: FC = () => {
+  // Defined the type of the App component, witch is a functional component
+
+  const [task, setTask] = useState<string>(''); // Defined the type of this state, witch is a string
+  const [deadline, setDeadline] = useState<number>(0); // Defined the type of this state, witch is a number
+  const [todo, setTodoList] = useState([]);
+
   return (
     <div className="App">
       <div className="header">
