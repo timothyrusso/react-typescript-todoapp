ff8a6a905af11aa7af3e2972ed61484176c583af
timothyrusso <russotimothy@live.it>
2022-07-25 08:13:47 +0200

    feat: todoList styling

--- src/Components/TodoTask.tsx
+++ src/Components/TodoTask.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { ITask } from '../Interfaces';
 
 interface Props {
+  // Describe the props that this component can accept
   task: ITask;
 }
 
 const TodoTask = ({ task }: Props) => {
   return (
-    <div>
-      {task.taskName} {task.deadline}
+    <div className="task">
+      <div className="content">
+        <span>{task.taskName}</span>
+        <span>{task.deadline}</span>
+      </div>
+      <button>X</button>
     </div>
   );
 };
